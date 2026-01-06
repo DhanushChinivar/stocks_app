3dab0503a900dccd30994207fa250174dcbba45b
Dhanush280 <73783265+DhanushChinivar@users.noreply.github.com>
2026-01-06 15:29:22 +1100

    Implemented Daily news summary feature

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import Image from "next/image";
-
 import NavItems from "@/components/NavItems";
 import UserDropdown from "@/components/UserDropdown";
 // import {searchStocks} from "@/lib/actions/finnhub.actions";
@@ -15,10 +14,10 @@ const Header = async ({ user }: { user: User }) => {
                     <Image src="/assets/icons/logo.svg" alt="Signalist logo" width={140} height={32} className="h-8 w-auto cursor-pointer" />
                 </Link>
                 <nav className="hidden sm:block">
-                    {<NavItems/>}
+                    {/*<NavItems initialStocks={initialStocks} />*/}
                 </nav>
 
-                {<UserDropdown />}
+                {  <UserDropdown user={user} /> }
             </div>
         </header>
     )
--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -12,7 +12,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import {useRouter} from "next/navigation";
 import {Button} from "@/components/ui/button";
 import {LogOut} from "lucide-react";
-// import NavItems from "@/components/NavItems";
+import NavItems from "@/components/NavItems";
 import {signOut} from "@/lib/actions/auth.actions";
 
 const UserDropdown = ({ user }: {user: User}) => {
@@ -30,12 +30,12 @@ const UserDropdown = ({ user }: {user: User}) => {
                     <Avatar className="h-8 w-8">
                         <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
                         <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                            {user?.name[0]}
+                            {user.name[0]}
                         </AvatarFallback>
                     </Avatar>
                     <div className="hidden md:flex flex-col items-start">
                         <span className='text-base font-medium text-gray-400'>
-                            {user?.name}
+                            {user.name}
                         </span>
                     </div>
                 </Button>
@@ -46,14 +46,14 @@ const UserDropdown = ({ user }: {user: User}) => {
                         <Avatar className="h-10 w-10">
                             <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
                             <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                                {user?.name[0]}
+                                {user.name[0]}
                             </AvatarFallback>
                         </Avatar>
                         <div className="flex flex-col">
                             <span className='text-base font-medium text-gray-400'>
-                                {user?.name}
+                                {user.name}
                             </span>
-                            <span className="text-sm text-gray-500">{user?.email}</span>
+                            <span className="text-sm text-gray-500">{user.email}</span>
                         </div>
                     </div>
                 </DropdownMenuLabel>
